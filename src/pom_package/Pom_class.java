5e4d2dade7f67c7ae1ce337f84f074b38f5a4781
prati-nikam <pratikshanikam77@gmail.com>
2022-07-08 11:21:24 +0530

    first mofication is done

--- src/pom_package/Pom_class.java
+++ src/pom_package/Pom_class.java
@@ -2,6 +2,6 @@ package pom_package;
 
 public class Pom_class {
   public static void main(String[] args) {
-	System.out.println("pom class newly created");
+	System.out.println("pom class newly created here");
 }
 }
